aec6d89ee9ee2f6f5d46c176923cffb49080d0bc
yuanzhou <yuanzhou19@gmail.com>
2023-01-06 23:55:48 -0500

    Cleanup and add spinner

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
-
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+// components in curly brackets are named export
+// no need to use curly brackets for default export
 import DocumentDropzone from './components/DocumentDropzone';
 
 
+/**
+ * The top level functional component of this App
+ */
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>DeepPhe-CR Demo App</h1>
-            </header>
 
-            <DocumentDropzone />
+        <header className="App-header">
+        <h1>DeepPhe-CR Demo App</h1>
+        </header>
+
+        <DocumentDropzone />
 
         </div>
     );
--- src/components/DocumentDropzone.js
+++ src/components/DocumentDropzone.js
@@ -1,12 +1,18 @@
-// useState is a Hook that lets you add React state to function components
-import React, {useState, useEffect} from "react";
+// components in curly brackets are named export
+// no need to use curly brackets for default export
+import {useState, useEffect} from "react";
 import {useDropzone} from 'react-dropzone';
+import {ThreeDots} from 'react-loader-spinner';
+import { trackPromise, usePromiseTracker } from 'react-promise-tracker';
 
-// Local
-//import DocumentSubmit from './DocumentSubmit';
 
-
-// Function component
+/**
+ * This function is a valid React component because it accepts a single “props” (which stands for properties) 
+ * object argument with data and returns a React element. We call such components "function components" 
+ * because they are literally JavaScript functions.
+ *
+ * @param {object} props The properties object
+ */
 function DocumentDropzone(props) {
     // Declare a new state variable `files` with an empty array as initial state
     // It returns a pair of values: the current state and a function that updates it
@@ -14,6 +20,26 @@ function DocumentDropzone(props) {
 
     const [result, setResult] = useState('');
     const [error, setError] = useState('');
+
+    // Function component of the spinner
+    const Spinner = props => {
+        const { promiseInProgress } = usePromiseTracker();
+
+        return (
+            <span className="spinner">
+            {promiseInProgress && 
+            <ThreeDots 
+            height="80" 
+            width="80" 
+            radius="9"
+            color="#4fa94d" 
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true} />}
+            </span>
+        );
+    };
 
     // const with curly brackets is object destructuring assignment from ES6 specifications
     // a shorthand way to initialize variables from object properties
@@ -31,92 +57,106 @@ function DocumentDropzone(props) {
             setError('');
 
             // `acceptedFiles` is an array and stores the details of each accepted
-            console.log(acceptedFiles)
+            console.log(acceptedFiles);
 
             acceptedFiles.forEach(file => {
                 // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
                 // no arguments
-                const reader = new FileReader()
+                const reader = new FileReader();
 
-                reader.onabort = () => console.log('file reading was aborted')
-                reader.onerror = () => console.log('file reading has failed')
+                reader.onabort = () => console.log('file reading was aborted');
+                reader.onerror = () => console.log('file reading has failed');
                 reader.onload = () => {
                     // Do whatever you want with the file contents
-                    const binaryStr = reader.result
-                    console.log(binaryStr)
+                    const binaryStr = reader.result;
+                    console.log(binaryStr);
 
                     // Add new property `preview` to each file object
-                    file.preview = binaryStr
+                    file.preview = binaryStr;
                 }
 
                 // Note, this readAsText() returns None (undefined).
                 reader.readAsText(file);
-            })
+            });
 
             // Update the state variable `files` using the `acceptedFiles` information 
-            setFiles(acceptedFiles)
+            setFiles(acceptedFiles);
         }
     });
 
-    // Fetch data from backend API
+    // Submit button handler
+    // Fetch data from backend API and store the text to react state variable
     function summarizeDocument() {
-        fetch("https://entity.api.hubmapconsortium.org/entities/0de3181b777383b7b918d4402021fb34")
-        .then(response => response.json())
-        .then(
-            (data) => {
-                // `data` is an object here, we need string
-                setResult(JSON.stringify(data));
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-                setError(error);
-            }
-        )
+        trackPromise(
+            fetch("https://entity.api.hubmapconsortium.org/entities/0de3181b777383b7b918d4402021fb34")
+            .then(response => response.json())
+            .then(
+                (data) => {
+                    // `data` is an object here, we need string
+                    setResult(JSON.stringify(data));
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setError(error);
+                }
+            )
+        );
 
-        console.log(result)
-        console.log(error)
+        console.log(result);
+        console.log(error);
     }
 
-    // Add `key`` property to avoid: Warning: Each child in a list should have a unique "key" prop
+    // Add `key` property to avoid: Warning: Each child in a list should have a unique "key" prop
     // Use <pre> to preserve the original preformatted text, 
     // in which structure is represented by typographic conventions rather than by elements.
     const DocumentPreview = files.map(file => (
         <div key={file.name} className="doc-preview">
+
         <header className="doc-preview-header">
         <span className="text-primary file-info">{file.name}, {file.size}</span>
-        <button type="submit" className="btn btn-primary" onClick={summarizeDocument}>Submit</button>
+        <button type="submit" className="btn btn-primary" onClick={summarizeDocument}>Summarize</button> 
+        <Spinner />
         </header>
 
         <div className="doc-content">
         <pre>{file.preview}</pre>
         </div>
+
         </div>
     ));
 
-    // Use <code> rather than <pre> to format
+    // Show the json payload of summarized doc or error message or nothing
     function summarizedDocument(text) {
-        if (result !== '') {
+        if (error) {
+            return (<div className="summarized-doc">{error.message}</div>);
+        } else if (result) {
             return (
                 <div className="summarized-doc">
+
                 <header className="doc-preview-header">
                 <span className="text-primary file-info">Review Document Summary</span>
                 </header>
                 
                 <div className="doc-content">
+                {/* Use <code> rather than <pre> to format */}
                 <code>{text}</code>
                 </div>
+
                 </div>
-            )
+            );
+        } else {
+            // Render nothing
+            return (null);
         }
     }
-        
-    let summarizedDoc = summarizedDocument(result);
 
-    // The spread syntax is denoted by three dots
+    // Final return of this DocumentDropzone (functional component)
     return (
         <div>
+
+        {/* The spread syntax is denoted by three dots */}
         <div {...getRootProps()} className="drop-zone">
         <input {...getInputProps()} />
         <p>Click to select patient note</p>
@@ -127,7 +167,8 @@ function DocumentDropzone(props) {
         </div>
 
         <div>
-        {summarizedDoc}
+        {/* A function can be defined and use by the expression, remember to add () */}
+        {summarizedDocument(result)}
         </div>
 
         </div>
